28ed156d06a2fe324a4a96805aaa62066627e7b2
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-05-31 22:41:47 +0000

    Add config for sass

---
+++ config.rb
@@ -0,0 +1,11 @@
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :expanded
+
+line_comments = false
+color_output = false
+preferred_syntax = :scss
