9fd9af0a1944255ee2717f6015d8beaa6b653a41
Ragnar Þór Valgeirsson <ragnar.valgeirsson@gmail.com>
2013-05-31 22:38:54 +0000

    Live reload all sass sub directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
         tasks: ['compass:dev']
       },
       livereload: {
-        files: ['css/*'],
+        files: ['css/*', 'sass/**/*.{scss,sass}'],
         options: {
           livereload: true
         }
